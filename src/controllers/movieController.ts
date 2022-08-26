f3fcb6608f8e65dd2d41209cd5c7ea573bf61af1
brucealberto <brucealberto2@gmail.com>
2022-08-26 10:49:15 -0300

    feat: function create on movieController

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -1,5 +1,16 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import { movieRepository } from '../repositories/movieRepository';
 
-export const create = async (req:Request, res:Response) => {
-//tetse
-}
+export const create = async (req: Request, res: Response) => {
+  const { title, genre } = req.body;
+  if (!title || !genre)
+    return res.status(400).json({ message: 'the field is empty.' });
+  try {
+    const newMovie = movieRepository.create({ title, genre });
+    await movieRepository.save(newMovie);
+    return res.status(201).json(newMovie);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
