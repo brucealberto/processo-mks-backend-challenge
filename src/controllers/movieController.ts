74f98e171129ccc262ca099a35d49fd508fb3ef2
brucealberto <brucealberto2@gmail.com>
2022-08-26 17:20:23 -0300

    feat: function update on movieController

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -17,23 +17,41 @@ export const create = async (req: Request, res: Response) => {
 
 export const listAll = async (req: Request, res: Response) => {
   try {
-    const movies = await movieRepository.find()
-    await movieRepository.save(movies)
-    return res.status(200).json(movies)
+    const movies = await movieRepository.find();
+    await movieRepository.save(movies);
+    return res.status(200).json(movies);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
-}
+};
 export const listById = async (req: Request, res: Response) => {
-  const {id} = req.params
+  const { id } = req.params;
   try {
-    const movieId = await movieRepository.findOne({where: {id: Number(id)}})
-    if(!movieId) return res.status(404).json({message: "movie not found."})
-    await movieRepository.save(movieId)
-    return res.status(200).json(movieId)
+    const movieId = await movieRepository.findOne({
+      where: { id: Number(id) },
+    });
+    if (!movieId) return res.status(404).json({ message: 'movie not found.' });
+    await movieRepository.save(movieId);
+    return res.status(200).json(movieId);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
-}
+};
+export const update = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const movieId = await movieRepository.findOne({
+      where: { id: Number(id) },
+    });
+    if (!movieId) return res.status(404).json({ message: 'movie not found.' });
+    await movieRepository.save(movieId);
+
+    await movieRepository.update(Number(id), req.body);
+    return res.status(200).json({message: "movie updated!"});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { create, listAll, listById } from "./controllers/movieController";
+import { create, listAll, listById, update } from "./controllers/movieController";
 
 const routes = Router()
 
 routes.post('/movies', create)
 routes.get('/movies', listAll)
 routes.get('/movies/:id', listById)
+routes.put('/movies/:id', update)
 
 export default routes
